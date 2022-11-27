39221a79e5d15e8a69f220f4f42e0338b2ec19db
Raien Shaheen <raienshaheen@gmail.com>
2022-11-27 03:03:16 -0500

    commit tree 67141a92293ceb3f6ea69588733cc91739a27c1b

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(street, street2){
+    return(street2 - street) + ('Blocks')
+}
+console.log(distanceFromHqInBlocks(5, 10))
+
+function distanceFromHqInFeet(street, street2){
+distanceFromHqInBlocks(street, street2);
+if(distanceFromHqInBlocks = street2 - street)
+    return(distanceFromHqInBlocks * 264) + 'Ft'
+}
+console.log(distanceFromHqInFeet(43, 50))
+
+function distanceTraveledInFeet(start, destination){
+    return((destination - start) * 264 +'Ft')
+}
+console.log(distanceTraveledInFeet(27, 52))
+
+function calculatesFarePrice(start, destination){
+    console.log(distanceTraveledInFeet(start, destination))
+    if(((destination - start) * 264) < '2500'){
+        return( ((destination - start) * 264) * .02 + 'Dollars')
+    } else {
+        return('Sorry, Over The Limit!')
+    }
+}
+    
+console.log(calculatesFarePrice(27, 52))
+console.log(calculatesFarePrice(50, 52))
